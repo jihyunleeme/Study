f147fd5e1efc37b6424a2ccaec8414d41203bacf
Jihyun Lee <jihyunlee.me@gmail.com>
2022-06-26 20:33:20 +0900

    stack

---
+++ Data Structure/03_stack.js
@@ -0,0 +1,24 @@
+/* Stack 스택 */
+
+/* 
+freeCodeCamp.org
+https://youtu.be/Gj5qBheGOEo
+*/
+
+// functions: push, pop, peek(retrieve, 검색하다), length
+
+let letters = [];
+
+let word = 'racecar';
+
+let rword = '';
+
+// put letters of word into stack - 문자를 스택에 넣기
+
+// pop off the stack in reverse order - 역순으로 스택 pop하기
+
+// << Create the stack >>- 스택 구현한기
+
+// Removes and returns the values at the end of the stack
+
+// Returns the value at the end of the stack
